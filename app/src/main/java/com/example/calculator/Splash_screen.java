e04fad476ee8c974029453df51400183ce1b729e
ramparshad <163500455+ramparshad@users.noreply.github.com>
2024-10-15 18:05:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculator/Splash_screen.java
@@ -0,0 +1,28 @@
+package com.example.calculator;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
